4bb52607273a6de11183feba7b82be254635c801
heunnajo <61869540+heunnajo@users.noreply.github.com>
2021-07-16 21:32:31 +0900

    Return member who's name is null by using COALESCE.

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -17,18 +17,18 @@ public class JpaMain {
 
         try {
             Member member = new Member();
-            member.setUsername("member1");
+            member.setUsername(null);
             member.setAge(10);
 
             em.persist(member);
 
-            String query =
-                    "select " +
-                            "case when m.age <= 10 then '학생요금' " +
-                            "     when m.age >= 60 then '경로요금' " +
-                            "     else '일반요금' "+
-                            "end " +
-                    "from Member m";
+            String query ="select coalesce(m.username, '이름 없는 회원') as username " + "from Member m ";
+//                    "select " +
+//                            "case when m.age <= 10 then '학생요금' " +
+//                            "     when m.age >= 60 then '경로요금' " +
+//                            "     else '일반요금' "+
+//                            "end " +
+//                    "from Member m";
 
             List<String> result = em.createQuery(query, String.class)
                     .getResultList();
